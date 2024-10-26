e3725a468fdf63a2c2343b0d77a019f33f848491
Adrian Velasquez <a.velasquezs@uniandes.edu.co>
2024-10-26 16:04:14 -0500

    Modification to HTTP GET and Environment

--- src/app/serie/serie.component.ts
+++ src/app/serie/serie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Serie } from './serie';
 import { dataSeries } from './dataSeries';
+import {SerieService} from './serie.service';
 
 @Component({
   selector: 'app-serie',
@@ -9,13 +10,17 @@ import { dataSeries } from './dataSeries';
 })
 export class SerieComponent implements OnInit {
 
+  constructor( private serieService: SerieService ) { }
   series: Array<Serie> = [];
-  constructor() { }
-  getSeriesList(): Array<Serie> {
-    return dataSeries;
+
+  getSeries() {
+    this.serieService.getSeries().subscribe(series => {
+      this.series = series;
+    });
   }
 
   ngOnInit() {
+    this.getSeries();
   }
 
 }
