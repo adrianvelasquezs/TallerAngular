2c06604a1f32bc910388f2b4e6997c652aca5445
Adrian Velasquez <a.velasquezs@uniandes.edu.co>
2024-10-26 16:15:19 -0500

    Modification to serie.service.spec.ts

--- src/app/serie/serie.service.spec.ts
+++ src/app/serie/serie.service.spec.ts
@@ -1,16 +1,24 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { SerieService } from './serie.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from '../app.component';
+import {SerieComponent} from './serie.component';
 
 describe('Service: Serie', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [AppComponent, SerieComponent],
       providers: [SerieService]
     });
   });
 
-  it('should ...', inject([SerieService], (service: SerieService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
 });
